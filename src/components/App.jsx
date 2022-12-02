fdf62228acc6d995aba7d63626766832ee8947e2
Natalia970970 <103364315+Natalia970970@users.noreply.github.com>
2022-12-02 16:25:18 +0200

    step2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+
+
+export class App extends Component {
+  state = {
+    good: 1,
+    neutral: 2,
+    bad: 3
+  }
+
+  
+
+  countTotalFeedback = (arrFeadbackValues) => {
+    return arrFeadbackValues.reduce((acc, value) => {
+      return acc + value
+    }, 0);
+  }
+
+
+  countPositiveFeedbackPercentage = (totalFeadbacks, positiveFeedbacks) => {
+      return Math.round((positiveFeedbacks / totalFeadbacks) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad} = this.state;
+    const arrFeadbackValues = Object.values(this.state);
+    const totalFeadbacks = this.countTotalFeedback(arrFeadbackValues);
+    const positiveFeedbacks = good;
+    const positivePersentage =
+    totalFeadbacks > 0
+        ? this.countPositiveFeedbackPercentage(
+          totalFeadbacks,
+            positiveFeedbacks
+          )
+        : 0;
+
+    return (
+      <div>
+          <h1>Please leave feadback</h1>
+          <button type='button on' onClick={() => {}}>Good</button>
+          <button type='button on' onClick={() => {}}>Neutral</button>
+          <button type='button on' onClick={() => {}}>Bad</button>
+          <h2>Statistics</h2>
+          <p>Good:{good}</p>
+          <p>Neutral:{neutral}</p>
+          <p>Bad:{bad}</p>
+          <p>Total:{totalFeadbacks}</p>
+          <p>Positive feadback:{positivePersentage}%</p>
+      </div>
+    );
+  }
+
 };
